d8456beab111a067a11ea3b82900e66683e13e97
ramonG05 <142957413+ramonG05@users.noreply.github.com>
2025-04-08 00:46:38 +0200

    Add files via upload

---
+++ python/loopforpractice.py
@@ -0,0 +1,9 @@
+prices = [10, 20, 30]
+
+total = 0
+for price in prices:
+    total += price
+print(f"Total: {total}")
+
+
+
---
+++ python/test.py
@@ -0,0 +1,9 @@
+q = 'qlq'
+
+print(q[1:-1])
+
+first = 'John'
+last = 'Smith'
+msg = f'{first} [{last}] is a coder'
+print(msg)
+
